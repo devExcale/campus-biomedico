56782d3a3538e6568a0699901ecb62f86068bc43
excale <escaccia@live.it>
2021-05-10 15:30:25 +0200

    Initial commit

---
+++ src/main/java/it/edu/faraday/campusbiomedico/CampusBiomedicoApplication.java
@@ -0,0 +1,13 @@
+package it.edu.faraday.campusbiomedico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusBiomedicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampusBiomedicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/edu/faraday/campusbiomedico/CampusBiomedicoApplicationTests.java
@@ -0,0 +1,13 @@
+package it.edu.faraday.campusbiomedico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusBiomedicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
